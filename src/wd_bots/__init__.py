9cdeb431d6b0daacdec533e13a0726c3b77c4437
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-29 00:57:47 +0300

    .

--- src/wd_bots/__init__.py
+++ src/wd_bots/__init__.py
@@ -2,9 +2,11 @@
 """
 !
 """
+import re
+from ..utils import lag_bot, logger
 from ..mk_cats.wd_login_wrap import log_in_wikidata
 from .wd_newapi_bot import WD_API
-from .req_bots_new import descriptions_wd, items_wd, labels_wd, sitelinks_wd
+from .req_bots_new import descriptions_wd, items_wd, sitelinks_wd
 from .utils.out_json import outbot_json, outbot_json_bot
 
 
@@ -24,14 +26,61 @@ class NewHimoAPIBot:
         # =======================
         items_wd_bot = items_wd.WD_Items(self.wdapi_new)
         descriptions_wd_bot = descriptions_wd.WD_Descriptions(self.wdapi_new)
-        labels_wd_bot = labels_wd.WD_Labels(self.wdapi_new, descriptions_wd_bot.Des_API)
         sitelinks_wd_bot = sitelinks_wd.WD_Sitelinks(self.wdapi_new)
         # ---
         self.New_API = items_wd_bot.New_API
         self.New_Mult_Des_2 = descriptions_wd_bot.New_Mult_Des_2
         self.Des_API = descriptions_wd_bot.Des_API
-        self.Labels_API = labels_wd_bot.Labels_API
         self.Sitelink_API = sitelinks_wd_bot.Sitelink_API
+
+    def Labels_API(
+        self, Qid, label, lang, ret, Or_Alii=False, change_des=False, number=0, nowait=False, tage="", remove=False
+    ):
+        # ---
+        if lag_bot.bad_lag(nowait):
+            return ""
+        # ---
+        if not Qid:
+            logger.debug("Labels_API Qid == '' ")
+            return False
+        # ---
+        if label == "" and not remove:
+            logger.debug("Labels_API label == '' and remove = False ")
+            return False
+        # ---
+        # save the edit
+        out = f'{Qid} label:"{lang}"@{label}.'
+        if number:
+            out = f'{number} {Qid} label:"{lang}"@{label}.'
+        r4 = self.session_post(
+            params={
+                "action": "wbsetlabel",
+                "id": Qid,
+                "language": lang,
+                "value": label,
+            },
+            tage=tage,
+        )
+        # ---
+        if not r4:
+            logger.debug("Labels_API r4 == {} ")
+            return False
+        # ---
+        text = str(r4)
+        if ("using the same description text" in text) and ("associated with language code" in text):
+            item2 = re.search(r"(Q\d+)", str(r4["error"]["info"])).group(1)
+            logger.debug(f"<<lightred>>API: same label item: {item2}")
+            if change_des is True:
+                self.find_des_and_replace(Qid, label, lang)
+            elif Or_Alii is True:
+                self.Alias_API(Qid, [label], lang, ret)
+        if ret:
+            return text
+        # ---
+        d = outbot_json(r4, fi=out, NoWait=nowait)
+        # ---
+        if d == "warn":
+            logger.warning(str(r4))
 
 
 __all__ = [
--- src/wd_bots/req_bots_new/__init__.py
+++ src/wd_bots/req_bots_new/__init__.py
@@ -4,11 +4,10 @@
 """
 
 # Expose the main API modules
-from . import descriptions_wd, items_wd, labels_wd, sitelinks_wd
+from . import descriptions_wd, items_wd, sitelinks_wd
 
 __all__ = [
     "items_wd",
-    "labels_wd",
     "descriptions_wd",
     "sitelinks_wd",
 ]
