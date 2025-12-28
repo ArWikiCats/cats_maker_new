1bbd402b7aff4ea0cd6197d9643c3cc693d67fdf
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-29 01:23:51 +0300

    Update __init__.py

--- src/mk_cats/__init__.py
+++ src/mk_cats/__init__.py
@@ -1,8 +1,7 @@
-from .mknew import ToMakeNewCat2222, ar_make_lab, no_work, one_cat
+from .mknew import ToMakeNewCat2222, ar_make_lab, no_work
 from .no_1 import make_category
 
 __all__ = [
-    "one_cat",
     "no_work",
     "ToMakeNewCat2222",
     "make_category",
