c42657fa06e77c96cb338cec8b44b7018e6083b3
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-29 02:37:25 +0300

    Refactor category processing and extract check_en_temps
    Moved check_en_temps to a new utils/check_en.py module for better separation of concerns. Replaced no_work with process_catagories and updated all references accordingly, improving naming clarity and logic flow. Updated __init__.py files to reflect new imports and exports.

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
 
-from .mk_cats import ToMakeNewCat2222, create_categories_from_list, ar_make_lab, no_work, make_category
+from .mk_cats import (ToMakeNewCat2222, create_categories_from_list, ar_make_lab, process_catagories, make_category, no_work,)
 
 __all__ = [
-    "ToMakeNewCat2222",
     "no_work",
+    "ToMakeNewCat2222",
+    "process_catagories",
     "create_categories_from_list",
     "make_category",
     "ar_make_lab",
--- src/mk_cats/__init__.py
+++ src/mk_cats/__init__.py
@@ -1,8 +1,9 @@
-from .mknew import ToMakeNewCat2222, create_categories_from_list, ar_make_lab, no_work
+from .mknew import ToMakeNewCat2222, create_categories_from_list, ar_make_lab, process_catagories, no_work
 from .create_category_page import make_category
 
 __all__ = [
     "no_work",
+    "process_catagories",
     "ToMakeNewCat2222",
     "create_categories_from_list",
     "make_category",
