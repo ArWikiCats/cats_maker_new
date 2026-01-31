59d3fbc750c597c868e8e892b6407b7257e41d78
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-31 04:31:34 +0300

    Refactor imports and dynamic logging setup in __init__.py
    Removed try-except block for imports and set up logging with the package name dynamically using pathlib. This improves clarity and ensures logging is correctly named based on the package context.

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,18 @@
-try:
-    from .logging_config import setup_logging
-    from .mk_cats import (
-        ToMakeNewCat2222,
-        ar_make_lab,
-        create_categories_from_list,
-        make_category,
-        no_work,
-        process_catagories,
-    )
 
-    setup_logging(level="DEBUG", name="src")
+from pathlib import Path
 
-except ImportError:
-    # Skip imports when running as a standalone package (e.g., during testing)
-    print("ImportError in cats_maker_new/src/__init__.py")
-    pass
+from .logging_config import setup_logging
+from .mk_cats import (
+    ToMakeNewCat2222,
+    ar_make_lab,
+    create_categories_from_list,
+    make_category,
+    no_work,
+    process_catagories,
+)
+
+name = Path(__file__).parent.name
+setup_logging(level="DEBUG", name=name)
 
 __all__ = [
     "no_work",
