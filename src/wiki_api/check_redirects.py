65f702f56a71277237e13297580ff561d15da32d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-31 08:04:14 +0300

    Fix instantiation of NEW_API in check_redirects.py
    Changed usage of NEW_API from a class reference to an instance to correctly call Find_pages_exists_or_not. This resolves a potential bug where the method would not be accessible without instantiating the class.

--- src/wiki_api/check_redirects.py
+++ src/wiki_api/check_redirects.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 def load_non_redirects(lang: str, page_titles: list) -> list:
     """Remove redirect pages from a list of page titles."""
     api = load_main_api(lang)
-    result = api.NEW_API.Find_pages_exists_or_not(page_titles, get_redirect=True)
+    result = api.NEW_API().Find_pages_exists_or_not(page_titles, get_redirect=True)
 
     non_redirects = [x for x, v in result.items() if v is True]  # and v != "redirect"
     return non_redirects
