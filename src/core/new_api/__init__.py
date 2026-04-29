993f585838e41a580d21ca02660a25720df89c57
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-29 03:36:39 +0300

    .

--- src/core/new_api/__init__.py
+++ src/core/new_api/__init__.py
@@ -1,11 +1,9 @@
 """ """
 
-from .api_utils import botEdit
 from ..client_wiki.factory import load_login_bot, load_main_api
 from .super_login import Login
 
 __all__ = [
-    "botEdit",
     "load_main_api",
     "load_login_bot",
     "Login",
--- src/core/new_api/super_login.py
+++ src/core/new_api/super_login.py
@@ -14,7 +14,7 @@ import requests
 from ...config import settings
 from .auth import AuthProvider
 from .cookies_bot import del_cookies_file, get_file_name
-from .api_utils.handel_errors import HandleErrors
+from .handel_errors import HandleErrors
 
 logger = logging.getLogger(__name__)
 
